38b24bcff1266a184200bf92e12a79e21279c27e
Khoa <khoa@wearebloom.com.au>
2023-09-28 18:03:24 +0700

    login with firebase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 	import Header from './Header.svelte';
 	import './styles.css';
 	import '../app.css';
-
 </script>
 
 <div class="app">
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<section id="main">
+	<h1 class="mt-10">Hello Admin</h1>
+</section>
