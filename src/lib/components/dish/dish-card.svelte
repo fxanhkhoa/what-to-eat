2ea965c7d4274ccf53cd6e619cf314e6fcc9be2d
fxanhkhoa <fxanhkhoa@gmail.com>
2023-12-10 11:36:25 +0700

    [WTE-006] bug no language found

--- src/lib/components/dish/dish-card.svelte
+++ src/lib/components/dish/dish-card.svelte
@@ -5,22 +5,23 @@
 	import DifficultyLevel from '../utility/difficulty-level.svelte';
 
 	export let dish: Dish;
-    export let selectedLanguage: string;
+	export let selectedLanguage: string;
 </script>
 
 <div
-	class="col-span-12 md:col-span-6 xl:col-span-6 2xl:col-span-4 grid grid-cols-12 shadow-lg rounded-lg rounded-r-full">
+	class="col-span-12 md:col-span-6 xl:col-span-6 2xl:col-span-4 grid grid-cols-12 shadow-lg rounded-3xl">
 	<div class="col-span-5 p-5">
 		<img src={dish.thumbnail} alt={dish.slug} class="object-cover rounded-lg w-full h-full" />
 	</div>
 	<div class="col-span-5 flex flex-col gap-2 py-5 pr-5">
-		<DifficultyLevel difficultLevel={dish.difficultLevel ?? DIFFICULT_LEVELS.EASY}/>
+		<DifficultyLevel difficultLevel={dish.difficultLevel ?? DIFFICULT_LEVELS.EASY} />
 
 		<h4 class="font-bold">
 			{dish.title?.find((t) => t?.lang === selectedLanguage)?.data}
 		</h4>
 		<span class="text-gray-400 text-left"
-			>{dish.shortDescription?.find((s) => s?.lang === selectedLanguage)?.data ?? ''}</span>
+			>{dish.shortDescription?.find((s) => s?.lang === selectedLanguage)?.data?.substring(0, 100) ??
+				''}</span>
 		<div class="flex gap-1">
 			<i class="fa-solid fa-stopwatch my-auto" />
 			<span class="ml-1">{(dish?.preparationTime ?? 0) + (dish?.cookingTime ?? 0)}</span>
@@ -30,20 +31,10 @@
 	<div class="col-span-2 relative">
 		<a
 			href="/dish/{dish.slug}"
-			class="relative w-full h-full inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-l rounded-r-full group border-gray-100">
+			class="relative w-full h-full inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-l rounded-r-3xl group border-gray-100">
 			<span
 				class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-				<svg
-					class="w-6 h-6"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
+				<i class="fa-solid fa-arrow-right" />
 			</span>
 			<span
 				class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease"
